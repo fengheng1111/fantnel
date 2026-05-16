bfe518715edcd0c11bea539b94d0ccedc430c228
风横 <3547694806@qq.com>
2026-05-16 11:23:00 +0800

    涅槃科技 [Hey*]新的 invalidTeam 检测方式

--- Nirvana.Heypixel/Play/SaClientboundSetPlayerTeamPacket.cs
+++ Nirvana.Heypixel/Play/SaClientboundSetPlayerTeamPacket.cs
@@ -11,8 +11,7 @@ public class SaClientboundSetPlayerTeamPacket : FPacket {
     
     public static readonly RegisterPacket RegisterPacket = new(EnumConnectionState.Play, EnumPacketDirection.ClientBound, 96, HeypixelProtocol.GameId, EnumProtocolVersion.V1206);
 
-    private string? _invalidTeamName; // 无效团队名称
-    private string? _teamName; // 当前团队名称
+    private string? _teamName; // 团队名称
     
     public override void ReadFromBuffer(BGameConnection connection, IByteBuffer buffer)
     {
@@ -26,16 +25,10 @@ public class SaClientboundSetPlayerTeamPacket : FPacket {
             return false;
         }
         
-        if (_invalidTeamName == null) {
-            if (_teamName.StartsWith("collideRule_")) {
-                _invalidTeamName = _teamName;
-            }
-        }
-
-        if (_teamName.Equals(_invalidTeamName)) {
+        if (_teamName.StartsWith("collideRule_")) {
             Log.Information("[Heypixel] Team: {0}", _teamName);
             return true;
-        }
+        } 
         
         return false;
     }
